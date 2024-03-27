a75ac614f37daccf9b4db0c93d9ddb7e22564114
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-27 19:08:34 +0900

    a

--- script/train.sh
+++ script/train.sh
@@ -22,6 +22,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
+ --use_original_seg_unet \
  --use_position_embedder \
  --n_classes 3 \
  --mask_res 256
