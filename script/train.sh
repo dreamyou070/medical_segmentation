31be8df09a8551c457919b58ec4286549a3b8cf6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-28 00:23:40 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -6,13 +6,13 @@
 # 5_pe_concat_segmentation_model_a_cross_focal_batch_norm_head
 # 6_pe_concat_segmentation_model_a_cross_focal_instance_norm_head
 
-port_number=50022
+port_number=50023
 category="medical"
 obj_name="leader_polyp"
 benchmark="PolypGen2021_save"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="2_pe_basic_segmentation_model_a_cross_focal_use_layer_norm"
+file_name="3_pe_basic_segmentation_model_a_cross_focal_use_instance_norm"
 #  --use_instance_norm
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -31,4 +31,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --use_position_embedder \
  --aggregation_model_a \
  --n_classes 2 \
- --mask_res 256
+ --mask_res 256 --use_instance_norm
