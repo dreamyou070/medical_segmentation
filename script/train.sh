cee5295536fc18f9e586e2deda474b03a888776d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-27 20:43:24 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -6,18 +6,18 @@
 # 5_pe_concat_segmentation_model_a_cross_focal_batch_norm_head
 # 6_pe_concat_segmentation_model_a_cross_focal_instance_norm_head
 
-port_number=50001
+port_number=50002
 category="medical"
 obj_name="leader_polyp"
 benchmark="bkai-igh-neopolyp"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="1_pe_basic_segmentation_model_a_cross_focal_layer_norm_head"
+file_name="2_pe_basic_segmentation_model_a_cross_focal_batch_norm_head"
 #
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
- --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
+ --train_unet --train_text_encoder --start_epoch 62 --max_train_epochs 200 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --train_data_path "/home/dreamyou070/MyData/anomaly_detection/medical/${obj_name}/${benchmark}/train" \
  --test_data_path "/home/dreamyou070/MyData/anomaly_detection/medical/${obj_name}/${benchmark}/test" \
@@ -32,4 +32,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --aggregation_model_a \
  --n_classes 3 \
  --mask_res 256 \
- --norm_type "layer_norm"
+ --norm_type "batch_norm"
