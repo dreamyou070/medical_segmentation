3b778f37bfef7c92ed5f422d2262d5a42f705be7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-27 19:04:32 +0900

    a

--- script/train.sh
+++ script/train.sh
@@ -24,6 +24,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --use_position_embedder \
  --n_classes 3 \
- --mask_res 256 \
- --norm_type "batchnorm" \
- --nonlinearity "relu"
+ --mask_res 256
