617d8023fcb98fc6d74cdde3e62789ae99160c86
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-29 00:00:36 +0900

    train

--- script/train.sh
+++ script/train.sh
@@ -14,13 +14,14 @@
 
 port_number=50501
 category="medical"
-obj_name="cardiac"
-benchmark="acdc"
+obj_name="brain"
+trigger_word="brain"
+benchmark="BraTS2020_Segmentation_256"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="1_absolute_pe_do_semantic_position_segmentation_model_a_cross_focal_use_batch_norm_query"
+file_name="1_absolute_pe_do_semantic_position_segmentation_model_a_cross_focal_use_batch_norm_query_gpu6"
 #  --use_instance_norm
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
@@ -29,7 +30,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --test_data_path "/home/dreamyou070/MyData/anomaly_detection/medical/${obj_name}/${benchmark}/test" \
  --resize_shape 512 \
  --latent_res 64 \
- --trigger_word "polyp" \
+ --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
