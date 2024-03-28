fa822f1f6cf1574081019a97eeb6b93f6861e7a2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-28 18:42:04 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -6,13 +6,13 @@
 # 5_absolute_pe_basic_segmentation_model_a_cross_focal_use_layer_norm_query_after_attn
 # 6_absolute_pe_basic_segmentation_model_a_cross_focal_use_instance_norm_query_after_attn --use_instance_norm
 
-port_number=50576
+port_number=50575
 category="medical"
 obj_name="leader_polyp"
 benchmark="bkai-igh-neopolyp"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="6_absolute_pe_basic_segmentation_model_a_cross_focal_use_instance_norm_query_after_attn"
+file_name="5_absolute_pe_basic_segmentation_model_a_cross_focal_use_layer_norm_query_after_attn"
 #  --use_instance_norm
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -33,5 +33,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --n_classes 3 \
  --mask_res 256 \
  --absolute_position_embedder \
- --use_instance_norm \
  --saving_query_after_attn
