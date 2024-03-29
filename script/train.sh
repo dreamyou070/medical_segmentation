9affa53c098b9235fabc1794799a8bb28062b5c3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-29 11:18:11 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -37,6 +37,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --use_position_embedder --do_semantic_position \
  --aggregation_model_a \
- --n_classes 14 \
- --mask_res 512 \
+ --n_classes 4 \
+ --mask_res 256 \
  --use_batchnorm
