4816dc63497c0879f771463a754ee1cb2702257e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-29 11:03:26 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -12,11 +12,11 @@
 # 8_absolute_pe_do_semantic_position_segmentation_model_a_cross_focal_use_batch_norm_query_after_attn --use_batchnorm
 
 
-port_number=50032
+port_number=50042
 category="medical"
-obj_name="cardiac"
-trigger_word="cardiac"
-benchmark="acdc"
+obj_name="abdomen"
+trigger_word="abdomen"
+benchmark="abdomen_without_bad_sample"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="2_absolute_pe_segmentation_model_a_cross_focal_use_batch_norm_query"
@@ -37,6 +37,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --use_position_embedder \
  --aggregation_model_a \
- --n_classes 4 \
- --mask_res 256 \
+ --n_classes 14 \
+ --mask_res 512 \
  --use_batchnorm
