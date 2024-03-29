4f2de4656365bb5b0dbe0b992dbbdf28cd5702a0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-29 16:01:28 +0900

    Update abdoman_check.py

--- abdoman_check.py
+++ abdoman_check.py
@@ -26,43 +26,27 @@ def main():
                               EnsureChannelFirstd(keys=["image", "label"]),
                               Orientationd(keys=["image", "label"], axcodes="RAS"),
                               Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest"), ),
-                              ScaleIntensityRanged(keys=["image"],
-                                                   a_min=-175, a_max=250, b_min=0.0, b_max=1.0,
-                                                   clip=True),
+                              ScaleIntensityRanged(keys=["image"],a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True),
                               CropForegroundd(keys=["image", "label"], source_key="image"), ])
-
     data_dir = r"/share0/dreamyou070/dreamyou070/MultiSegmentation/result/medical/data"
-    split_json = "dataset_0.json"
+    split_json = "dataset_1.json"
     datasets = os.path.join(data_dir, split_json)
     val_files = load_decathlon_datalist(datasets, True, "validation")
     val_ds = CacheDataset(data=val_files,
                           transform=val_transforms,
                           cache_num=6, cache_rate=1.0, num_workers=4)
-    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
+    #val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
+    print(f' step 3. Load the model')
+    print(f' len of val_ds = {len(val_ds)}')
+    for i in range(len(val_ds)):
+        sample = val_ds.__getitem__(i)
+        image = sample["image"] # arr
+        label = sample["label"] # arr
+        print(f'image.shape = {image.shape} | label.shape = {label.shape}')
+        print(f'image = {type(image)} | label = {type(label)}')
+        print(f'image max = {image.max()} | label max = {label.max()}')
 
 
-    print(f' step 3. Check data shape and visualize')
-    slice_map = {"img0035.nii.gz": 170,
-        "img0036.nii.gz": 230,
-        "img0037.nii.gz": 204,
-        "img0038.nii.gz": 204,
-        "img0039.nii.gz": 204,
-        "img0040.nii.gz": 180,}
-    case_num = 0
-    img_name = os.path.split(val_ds[case_num]["image"].meta["filename_or_obj"])[1]
-    img = val_ds[case_num]["image"]
-    label = val_ds[case_num]["label"]
-    img_shape = img.shape
-    label_shape = label.shape
-    print(f"image shape: {img_shape}, label shape: {label_shape}")
-    plt.figure("image", (18, 6))
-    plt.subplot(1, 2, 1)
-    plt.title("image")
-    plt.imshow(img[0, :, :, slice_map[img_name]].detach().cpu(), cmap="gray")
-    plt.subplot(1, 2, 2)
-    plt.title("label")
-    plt.imshow(label[0, :, :, slice_map[img_name]].detach().cpu())
-    plt.show()
 
 
 if __name__ == '__main__':
