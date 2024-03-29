acd119213dd94128f995c113772e7373bd2b6d78
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-29 11:50:44 +0900

    Update evaluate.py

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -43,7 +43,10 @@ def evaluation_check(segmentation_head, dataloader, device, text_encoder, unet, 
                 res = int(query.shape[1] ** 0.5)
                 q_dict[res] = reshape_batch_dim_to_heads(query) # 1, res,res,dim
             x16_out, x32_out, x64_out = q_dict[16], q_dict[32], q_dict[64]
-            masks_pred = segmentation_head(x16_out, x32_out, x64_out) # 1,4,128,128
+            if args.use_init_query:
+                masks_pred = segmentation_head(x16_out, x32_out, x64_out,x_init = latents) # 1,4,128,128
+            else :
+                masks_pred = segmentation_head(x16_out, x32_out, x64_out)
             #######################################################################################################################
             # [1] pred
             class_num = masks_pred.shape[1]  # 4
