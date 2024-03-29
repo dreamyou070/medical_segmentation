d86326fe0e3563809dfb8971f799c58581a9282d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-29 22:20:16 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -8,14 +8,14 @@
 # 4_absolute_pe_segmentation_model_c_cross_focal_use_batch_norm_query
 # 6_absolute_pe_segmentation_model_b_cross_focal_use_batch_norm_query
 
-port_number=56603
+port_number=56644
 category="medical"
-obj_name="brain"
-trigger_word="brain"
-benchmark="BraTS2020_Segmentation_256"
+obj_name="abdomen"
+trigger_word="abdomen"
+benchmark="abdomen_re"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="3_absolute_pe_segmentation_model_c_cross_focal_use_batch_norm_query"
+file_name="4_absolute_pe_segmentation_model_c_cross_focal_use_batch_norm_query"
 # --use_instance_norm
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -32,7 +32,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --use_position_embedder \
- --aggregation_model_c \
+ --aggregation_model_b \
  --n_classes 4 \
  --mask_res 256 \
  --use_batchnorm
