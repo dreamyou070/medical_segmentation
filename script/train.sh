fe114c1b006c170fdcd8a072882f78b3d2162b1d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-30 13:59:58 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -15,7 +15,7 @@ layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="4_absolute_pe_segmentation_model_c_cross_backgroud_non_focal_use_batch_norm_query_data_aug"
 # --use_instance_norm
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
