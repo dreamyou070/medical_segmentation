fbe885f2055a1e594c46d2bb0ad89031c5bb5988
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-30 22:51:56 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -6,14 +6,14 @@
 # 4_absolute_pe_segmentation_model_c_cross_focal_use_batch_norm_query
 # 6_absolute_pe_segmentation_model_b_cross_focal_use_batch_norm_query
 
-port_number=51242
+port_number=51241
 category="medical"
 obj_name="brain"
 trigger_word="brain"
 benchmark="BraTS2020_Segmentation_128"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="2_new_data_absolute_pe_segmentation_model_c_cross_monai_focal_loss"
+file_name="1_new_data_absolute_pe_segmentation_model_c_use_dice_ce_loss"
 # --use_instance_norm
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -29,9 +29,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --use_position_embedder \
  --aggregation_model_c \
  --n_classes 4 \
  --mask_res 128 \
  --use_batchnorm \
- --use_monai_focal_loss
+ --use_dice_ce_loss
