d821c286d6e1fdef6e7096df2f2c55bf5c601d28
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-31 11:50:47 +0900

    padding mask

---
+++ data/brats2020_padding.py
@@ -0,0 +1,56 @@
+import os
+import h5py
+import numpy as np
+import nibabel as nib
+import glob
+from tifffile import imsave
+from sklearn.preprocessing import MinMaxScaler
+from PIL import Image
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+
+# [1] scaler to change image pixel range
+# [2]
+# [3] slicing on slice axis
+# [4] combine and reshape to remove bad point (240 -> 128)
+
+def scaling_img(raw_img, scaler):
+    raw = raw_img.reshape(-1, raw_img.shape[-1])  # [all pixel values, angles]
+    raw = scaler.fit_transform(raw)  # set value from 0 ~ 1
+    raw_img = raw.reshape(raw_img.shape)  # 240,240,155
+    return raw_img
+
+def main():
+
+    # [1] flair
+    # [2] t1ce
+    # [3] t2
+    # [4] mask
+
+    folder_128 = r'/home/dreamyou070/MyData/anomaly_detection/medical/brain/BraTS2020_Segmentation_128'
+    folder_128_image = os.path.join(folder_128, 'image_128')
+    folder_128_mask = os.path.join(folder_128, 'mask_128')
+    folder_256_mask = os.path.join(folder_128, 'mask_256')
+    os.makedirs(folder_256_mask, exist_ok = True)
+
+    test_folder_128 = os.path.join(folder_128, 'test')
+    test_folder_128_image = os.path.join(test_folder_128, 'image_128')
+    test_folder_128_mask = os.path.join(test_folder_128, 'mask_128')
+    test_folder_256_mask = os.path.join(test_folder_128, 'mask_256')
+    os.makedirs(test_folder_256_mask, exist_ok = True)
+
+    train_mask_files = os.listdir(folder_128_mask)
+
+    for i, train_mask_file in enumerate(train_mask_files) :
+        train_mask_file_dir = os.path.join(folder_128_mask, train_mask_file)
+        train_mask_arr = np.load(train_mask_file_dir)
+        trg_size = 256
+        trg_mask = np.zeros((trg_size,trg_size))
+
+        padding = (trg_size - 128)//2
+        trg_mask[padding:padding+128,padding:padding+128] = train_mask_arr
+        np.save(os.path.join(folder_256_mask, train_mask_file), trg_mask)
+
+if __name__ == '__main__' :
+    main()
