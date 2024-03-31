519c3f2ffd504c33c8082e63531fccb9a83430a1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-31 13:09:09 +0900

    Update brats_datacheck.py

--- data/brats_datacheck.py
+++ data/brats_datacheck.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
 
-trg_folder = '/home/dreamyou070/MyData/anomaly_detection/medical/brain/BraTS2020_Segmentation/train/res_128/mask_256'
+trg_folder = '/home/dreamyou070/MyData/anomaly_detection/medical/brain/BraTS2020_Segmentation/test/res_256/mask_128'
 files = os.listdir(trg_folder)
 for file in files :
     file_path = os.path.join(trg_folder, file)
     mask_arr = np.load(file_path)
-    print(f'mask shape (256,256) : {mask_arr.shape}')
+    print(f'mask shape (128,128) : {mask_arr.shape}')
