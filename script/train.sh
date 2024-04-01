47d4cdf02bce469683cdc441f7a0ca663838e264
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-01 11:24:43 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -14,19 +14,20 @@ benchmark="BraTS2020_Segmentation_256"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
 #file_name="4_new_data_pe_segmentation_model_c_use_dice_ce_loss_only_class_1"
-file_name="4_new_data_pe_segmentation_model_d_use_dice_ce_loss_text_guided"
+file_name="1_new_data_absolute_pe_segmentation_model_c_use_dice_ce_loss_network_dim_128"
 # --use_instance_norm
 #'up_blocks_1_attentions_2_transformer_blocks_0_attn2',
 #'up_blocks_2_attentions_2_transformer_blocks_0_attn2', \
 # --binary_test
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --train_data_path "/home/dreamyou070/MyData/anomaly_detection/medical/${obj_name}/${benchmark}/train" \
  --test_data_path "/home/dreamyou070/MyData/anomaly_detection/medical/${obj_name}/${benchmark}/test" \
+ --network_dim 128 --network_alpha 64 \
  --resize_shape 512 \
  --latent_res 64 \
  --trigger_word "${trigger_word}" \
@@ -34,7 +35,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --aggregation_model_d \
+ --aggregation_model_c \
  --n_classes 4 \
  --mask_res 256 \
  --use_batchnorm \
