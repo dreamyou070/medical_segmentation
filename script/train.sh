fe6210139171d5f3a176374231f8b246810bc261
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-03 14:00:01 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -17,7 +17,7 @@ file_name="1_segment_with_head_model"
 # --use_instance_norm
 # --binary_test
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
